5f7923e3712b1ff65f8e31a2f1dae5192c330663
Cam Sweeney <cpsweene@gmail.com>
2017-09-13 14:53:16 -0700

    use select for toggleSources

--- src/components/feed/toggleSources.vue
+++ src/components/feed/toggleSources.vue
@@ -1,42 +1,62 @@
 <template>
-
- <q-tabs v-model="activeSource" align='center'> <!-- class='toolbar justified bg-tertiary text-primary'> -->
+<!--  <q-tabs v-model="activeSource" align='center'>
     <q-tab slot='title' name='all' icon='all' label='all'></q-tab>
 
     <q-tab v-for='source in sources':key='source'
       slot='title'
       :name='source'
       icon="filter_center_focus"
-      :label='source'>
+      :label='source'
+      color='white'>
     </q-tab>
   </q-tabs>
+ -->
+
+  <q-select
+      dark
+      filter
+      align='center'
+      color='primary'
+      v-model="activeSource"
+      :options="selectOptions"
+    />
 
 </template>
 
 <script>
-  export default {
-    name: 'toggle-source',
-    props: ['sources'],
-    ready () {
-      this.activeSource = this.selectedSource
-    },
-    data () {
-      return {
-        activeSource: null
+import { QSelect } from 'quasar'
+import { mapState } from 'vuex'
+export default {
+  name: 'toggle-source',
+  props: ['sources'],
+  components: {
+    QSelect
+  },
+  data () {
+    return {
+      activeSource: this.selectedSource || 'all'
+    }
+  },
+  computed: {
+    ...mapState({
+      selectedSource: state => state.player.selectedSource
+    }),
+    selectOptions () {
+      let options = [{label: 'all', value: 'all'}]
+      for (var i = 0; i < this.sources.length; i++) {
+        let source = this.sources[i]
+        options.push({label: source, value: source})
       }
-    },
-    computed: {
-      selectedSource () {
-        return this.$store.state.player.selectedSource
-      }
-    },
-    watch: {
-      activeSource () {
-        console.log('setting source')
-        this.$store.commit('player/setSelectedSource', this.activeSource)
-      }
+      return options
+    }
+  },
+  watch: {
+    activeSource () {
+      console.log('setting source')
+      this.$store.commit('player/setSelectedSource', this.activeSource)
     }
   }
+}
 </script>
 
 <style>
